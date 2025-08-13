041ad3223f321cf4e54cc418b474ef4d3f86e4d4
Ashkan-Aali <ashkan.aali2023@gmail.com>
2025-08-13 15:09:53 +0330

    get users data and implementing add user page

--- src/layout/content/index.jsx
+++ src/layout/content/index.jsx
@@ -1,9 +1,10 @@
-import { Routes , Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import UsersPage from "../../pages/users";
 import PostsPage from "../../pages/posts";
 import CommentsPage from "../../pages/comments";
 import TasksPage from "../../pages/tasks";
 import GalleriesPage from "../../pages/galleries";
+import AddUserPage from "../../pages/users/add-user";
 
 const Content = () => {
   return (
@@ -11,10 +12,12 @@ const Content = () => {
       <div className="p-4">
         <Routes>
           <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/add-user" element={<AddUserPage />} /> 
           <Route path="/posts" element={<PostsPage />} />
           <Route path="/comments" element={<CommentsPage />} />
           <Route path="/tasks" element={<TasksPage />} />
           <Route path="/gallery" element={<GalleriesPage />} />
+          <Route path="/" element={<Navigate to={"/users"} />} />
         </Routes>
       </div>
     </div>
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,9 +1,38 @@
-const UsersPage = () => {
-    return (
-        <div>
-            UsersPage
-        </div>
-    );
-};
+import { useEffect, useState } from "react";
+import { getUsersServices } from "../../services/users";
+import { FaUserPlus } from "react-icons/fa";
+import UsersTable from "./_components/UsersTable";
+import { Link } from "react-router";
 
+
+const UsersPage = () => {
+  const [users, setUsers] = useState([]);
+
+  const handleGetUsers = async () => {
+    try {
+      const data = await getUsersServices();
+      setUsers(data || []);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetUsers();
+  }, []);
+
+  return (
+    <div className="space-y-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">مدیریت کاربران</h1>
+        <Link to={"/users/add-user"} className="bg-blue-400 text-white px-4 py-2 rounded-md flex items-center gap-2">
+          <FaUserPlus />
+          <span>افزودن کاربر</span>
+        </Link>
+      </div>
+
+      <UsersTable users={users} />
+    </div>
+  );
+};
 export default UsersPage;
